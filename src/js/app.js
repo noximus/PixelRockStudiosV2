1ef77758434832d075b9c4fd318ea7c7a1fe1236
Henry Richardson <hrichardson83@gmail.com>
2017-09-07 11:51:29 -0400

    fixed the navigation and switched footer links

--- src/js/app.js
+++ src/js/app.js
@@ -6,4 +6,23 @@ angular.module('PixelRockStudios', [
 
 .config(function($routeProvider) {
   $routeProvider.when('/', {templateUrl:'home.html',  reloadOnSearch: false});
+  $routeProvider.when('/portfolio', {templateUrl:'portfolio.html',  reloadOnSearch: false});
+  $routeProvider.when('/project', {templateUrl:'project.html',  reloadOnSearch: false});
+  $routeProvider.when('/contact', {templateUrl:'contact.html',  reloadOnSearch: false});
 });
+/*.config(function($stateProvider) {
+  var helloState = {
+    name: 'Home',
+    url: '/',
+    template: 'home.html'
+  }
+
+  var portfolioState = {
+    name: 'portfolio',
+    url: '/portfolio',
+    template: '<h3>Its the UI-Router hello world app!</h3>'
+  }
+
+  $stateProvider.state(helloState);
+  $stateProvider.state(portfolioState);
+});*/
